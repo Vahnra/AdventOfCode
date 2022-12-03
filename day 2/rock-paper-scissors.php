d45d22d8e212ae8c11f168b4d53e549842368536
Vahnra <102360714+Vahnra@users.noreply.github.com>
2022-12-03 17:37:44 +0100

    day2 done

---
+++ day 2/rock-paper-scissors.php
@@ -0,0 +1,5 @@
+<?php
+
+// foreach(file('input.txt')as$l){$s+=(1+strpos("BXCYAZAXBYCZCXAYBZ",$l[0].$l[2])/2);}echo$s;
+
+foreach(file('input.txt')as$l){$s+=(1+strpos("BXCXAXAYBYCYCZAZBZ",$l[0].$l[2])/2);}echo$s;
